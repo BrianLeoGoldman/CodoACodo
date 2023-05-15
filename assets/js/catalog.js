c8e212508a492b3a5b6c6e66c6ba565506a70fc5
Brian Goldman <leonel89011@gmail.com>
2023-05-15 16:48:14 -0300

    Fetching API url working

---
+++ assets/js/catalog.js
@@ -0,0 +1,31 @@
+import { storage } from "./storage.js";
+const { createApp } = Vue;
+
+let url = "https://sheetdb.io/api/v1/f7bl7f40k06yl"
+let imgPath = "../media/images/catalog/"
+const idParent = "catalog";
+const app = {
+    data: () => {
+        return {
+            catalog: []
+        };
+    },
+    methods: {
+        getCatalog: async function () {
+            try {
+                const result = await fetch(url);
+                const data = await result.json();
+                data.forEach(product => {
+                    product.img = imgPath + product.img
+                    this.catalog.push(product)
+                    console.log(product)
+                });
+            } catch(error) {
+                console.log(error);
+            }
+        },
+    },
+};
+
+let catalogComponent = createApp(app);
+catalogComponent.mount(`#${idParent}`);

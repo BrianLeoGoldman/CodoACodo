d21f17f0305df09fced0241543950370bd355c1e
Brian Goldman <leonel89011@gmail.com>
2023-05-21 20:26:31 -0300

    Temporal fix for max request reached on Google Sheets DB API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,15 @@
 import { storage } from "./storage.js";
 const { createApp } = Vue;
 
-let url = "https://sheetdb.io/api/v1/9a14vle9mievy"
+// let url = "https://sheetdb.io/api/v1/9a14vle9mievy"
+let url = "../assets/data/promos.json"
 let imgPath = "./assets/media/images/promos/"
 const idParent = "app";
 const app = {
     data: () => {
         return {
             loading: true,
-            packs: [
-                /* {name: "PACK 1", content: "SHIRT + PANTS + SOCKS", price: "$1000", img: "./assets/media/images/promos/Combo1.jpg"},
-                {name: "PACK 2", content: "HOODIE + CAP + SCARF", price: "$2000", img: "./assets/media/images/promos/Combo2.jpg"},
-                {name: "PACK 3", content: "JACKET + GLOVES + SOCKS", price: "$3000", img: "./assets/media/images/promos/Combo3.jpg"} */
-            ],
+            packs: [],
         };
     },
     methods: {
@@ -25,7 +22,6 @@ const app = {
                     this.packs.push(promo)
                 });
                 const shuffled = this.packs.sort(() => 0.5 - Math.random());
-                console.log(shuffled)
                 this.packs = shuffled.slice(0, 3);
                 this.loading = !this.loading
             } catch(error) {

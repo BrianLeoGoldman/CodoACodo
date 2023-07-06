025e3c2ce4e441de82af1e106419d1f484bcfb19
Brian Goldman <leonel89011@gmail.com>
2023-07-06 00:18:44 -0300

    Email delivery working

--- assets/js/forgot.js
+++ assets/js/forgot.js
@@ -12,10 +12,34 @@ button.addEventListener('click', (e) => {
             text: 'Please enter your registration email',
         });
     } else {
-        Swal.fire({
-            icon: 'success',
-            title: 'Check your email box!',
-            html:'Go to ' + '<a href="../pages/login.html">Log In</a>',
-        });
+        const options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                //'X-CSRFToken': csrftoken,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `email=${email}`
+        }
+        fetch('http://127.0.0.1:8000/marylu/forgot/', options)
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                let success_msg = 'Email delivery successful'
+                if (data === success_msg) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Check your email box!',
+                        html:'Go to ' + '<a href="../pages/login.html">Log In</a>',
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Email delivery failed',
+                        html:'Please check the email direction and try again',
+                    });
+                }
+            })
+            .catch(error => console.log(error));
     }
 });
